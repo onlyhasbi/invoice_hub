9cf9e8fe3e971f75a82a0d3224597bc43f8e3de5
Muhammad Hasbi <hasbi@akasia.id>
2025-02-18 16:59:07 +0800

    bugfix: search issue

--- src/app/invoices/list/page.tsx
+++ src/app/invoices/list/page.tsx
@@ -67,7 +67,7 @@ function ListInvoice() {
             );
 
             for (const [key, filterValue] of filterEntries) {
-              if (!itemLower[key].startsWith(filterValue.toLowerCase())) {
+              if (!itemLower[key].includes(filterValue.toLowerCase())) {
                 return false;
               }
             }
